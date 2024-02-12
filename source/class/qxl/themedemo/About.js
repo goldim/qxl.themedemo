9b95cc02a483400e248c5e07166db6d5654f6d55
goldim <goldim1993@gmail.com>
2024-02-12 12:12:41 +0300

    Removed decorator beacuse it doesn't work and it is not needed.

--- source/class/qxl/themedemo/About.js
+++ source/class/qxl/themedemo/About.js
@@ -46,11 +46,10 @@ qx.Class.define("qxl.themedemo.About", {
 
       var url = qx.util.ResourceManager.getInstance().toUri(this._aboutTextUrl);
 
-      var textBox = (this.textBox = new qx.ui.embed.ThemedIframe(url).set({
-        decorator: "input",
+      var textBox = new qx.ui.embed.ThemedIframe(url).set({
         width: 580,
-        height: 350,
-      }));
+        height: 350
+      });
 
       this.add(textBox, { flex: 1 });
       this.add(
